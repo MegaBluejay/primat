4a37e91830d12e3e3f89ab983acfc81b31929a0c
MegaBluejay <dmoiseev2011@gmail.com>
2022-04-02 15:23:59 +0300

    fix test

--- primat/test.py
+++ primat/test.py
@@ -1,10 +1,21 @@
 from math import sin
-from scipy.optimize import minimize_scalar
+from .primat import *
+
+f = lambda x: sin(x) - log(x**2) - 1
 
 
-def f1(x):
-    return sin(x) * x**3
+def test(n):
+    res = {}
+    for minimizer in [DihotMinimizer, GoldenMinimizer, FibMinimizer, ParabolaMinimizer, BrentMinimizer]:
+        try:
+            res[minimizer.__name__] = minimizer(f, 8, 13.5, 10 ** (-5), max_calls=n).minimize()
+        except Exception:
+            res[minimizer.__name__] = "failed"
+    return res
 
 
-def test(minimizer, f, a, b, eps):
-    return abs(minimize_scalar(f, bounds=(a, b)).x - minimizer(f, a, b, eps).minimize().x) < eps
+table = {i: test(i) for i in range(4, 50)}
+for i in range(4, 50):
+    print(i)
+    for name, res in table[i].items():
+        print(" ", name, res)
