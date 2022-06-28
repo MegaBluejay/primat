297f907ca78bff2bdfa7897fe5acd1a18d97b947
MegaBluejay <dmoiseev2011@gmail.com>
2022-06-28 18:25:14 +0300

    add second gen

--- primat/lab3/gen.py
+++ primat/lab3/gen.py
@@ -1,2 +1,13 @@
+from random import choices
+
 def gilbert(n):
     return [[1/(i + j + 1) for j in range(n)] for i in range(n)]
+
+def qq(n):
+    x = list(range(-4, 1))
+    res = []
+    for i in range(n):
+        q = list(choices(x, k=n-1))
+        q.insert(i, -sum(q) + 10**-n)
+        res.append(q)
+    return res
