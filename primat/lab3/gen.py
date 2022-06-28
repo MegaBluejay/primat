4e9791a92ec2b469f3883d20185119255c75505d
MegaBluejay <dmoiseev2011@gmail.com>
2022-06-29 00:37:59 +0300

    fix diagonalish gen

--- primat/lab3/gen.py
+++ primat/lab3/gen.py
@@ -1,13 +1,24 @@
 from random import choices
 
+from csr import *
+
 def gilbert(n):
     return [[1/(i + j + 1) for j in range(n)] for i in range(n)]
 
-def qq(n):
-    x = list(range(-4, 1))
-    res = []
+def qq(n, k):
+    q = list(range(-4, 1))
+    v, c, r, f = [], [], [], []
     for i in range(n):
-        q = list(choices(x, k=n-1))
-        q.insert(i, -sum(q) + 10**-n)
-        res.append(q)
-    return res
+        r.append(len(v))
+        row = list(choices(q, k=n-1))
+        row.insert(i, -sum(row) + 10**(-k))
+        fv = 0
+        for j, w in enumerate(row):
+            if not w:
+                continue
+            v.append(w)
+            c.append(j)
+            fv += w * (j + 1)
+        f.append(fv)
+    r.append(len(v))
+    return Csr((v, c, r)), f
