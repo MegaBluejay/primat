06234ecb9dbc53ea484d7598bec481cbd9eb1378
MegaBluejay <dmoiseev2011@gmail.com>
2022-06-29 09:27:58 +0300

    jacobi v2

--- primat/lab4/jacobi.py
+++ primat/lab4/jacobi.py
@@ -1,5 +1,5 @@
 from bisect import bisect_right, bisect_left
-from math import copysign, sqrt, inf
+from math import copysign, sqrt, inf, atan
 
 from primat.lab3.csr import Csr
 
@@ -14,79 +14,43 @@ def non_diag(a):
 
 
 def next_jac(a):
-    q = max(non_diag(a), key=lambda t: abs(t[1]))[0]
+    q = max(non_diag(a), key=lambda t: abs(t[1]), default=(-1, 0))[0]
     j, k = sorted([bisect_right(a.r, q) - 1, a.c[q]])
     if a[j, j] == a[k, k]:
-        sin, cos = 1 / sqrt(2), 1 / sqrt(2)
+        s, c = 1 / sqrt(2), 1 / sqrt(2)
     else:
         tau = (a[j, j] - a[k, k]) / (2 * a[j, k])
-        tan = copysign(1 / (abs(tau) + sqrt(1 + tau**2)), tau)
-        cos = 1 / sqrt(1 + tau**2)
-        sin = tan * cos
+        c = 1 / sqrt(1 + tau**2)
+        s = copysign(1 / (abs(tau) + sqrt(1 + tau**2)), tau) * c
 
-    jj = cos**2 * a[j, j] - 2 * sin * cos * a[j, k] + sin**2 * a[k, k]
-    kk = sin**2 * a[j, j] + 2 * sin * cos * a[j, k] + cos**2 * a[k, k]
-    jk = (cos**2 - sin**2) * a[j, k] + sin * cos * (a[k, k] - a[j, j])
-
-    v, c, r = [], [], [0]
+    b = Csr(([], [], [0], a.n))
     for i in range(a.n):
-        if i == j:
-            for m in range(a.m):
-                if m == j:
-                    x = jj
-                elif m == k:
-                    x = jk
-                else:
-                    x = cos * a[j, m] - sin * a[k, m]
-                if not x:
-                    continue
-                v.append(x)
-                c.append(m)
-        elif i == k:
-            for m in range(a.m):
-                if m == j:
-                    x = jk
-                elif m == k:
-                    x = kk
-                else:
-                    x = sin * a[j, m] + cos * a[k, m]
-                if not x:
-                    continue
-                v.append(x)
-                c.append(m)
-        else:
-            start, end = a.r[i : i + 2]
-            xj = cos * a[j, i] - sin * a[k, i]
-            xk = sin * a[j, i] + cos * a[k, i]
-            for x, m in zip(a.v[start:end], a.c[start:end]):
-                if m == j:
-                    x = xj
-                elif m == k:
-                    x = xk
-                v.append(x)
-                c.append(m)
-            qj = bisect_left(c, j, r[-1])
-            if qj == len(c) or c[qj] != j:
-                c.insert(qj, j)
-                v.insert(qj, xj)
-            qk = bisect_left(c, k, r[-1])
-            if qk == len(c) or c[qk] != k:
-                c.insert(qk, k)
-                v.insert(qk, xk)
-        r.append(len(v))
-    return Csr((v, c, r, a.m))
+        for m in range(i):
+            x = b[m, i]
+            if x:
+                b.v.append(x)
+                b.c.append(m)
+        for m in range(i, a.n):
+            if [i, m] == [j, j]:
+                x = c**2 * a[j, j] - 2 * s * c * a[j, k] + s**2 * a[k, k]
+            elif [i, m] == [k, k]:
+                x = s**2 * a[j, j] + 2 * s * c * a[j, k] + c**2 * a[k, k]
+            elif [i, m] == [j, k]:
+                x = (c**2 - s**2) * a[j, k] + s * c * (a[k, k] - a[j, j])
+            elif j in [i, m]:
+                x = c * a[j, m] - s * a[k, m]
+            elif k in [i, m]:
+                x = s * a[j, m] + c * a[k, m]
+            else:
+                x = a[i, m]
+            if x:
+                b.v.append(x)
+                b.c.append(m)
+        b.r.append(len(b.v))
+    return b
 
 
 def jacobi(a, eps):
-    while abs(max(non_diag(a), key=lambda t: abs(t[1]))[1]) >= eps:
+    while abs(max(non_diag(a), key=lambda t: abs(t[1]), default=(-1, 0))[1]) >= eps:
         a = next_jac(a)
     return [a[i, i] for i in range(a.n)]
-
-
-import numpy as np
-
-a = np.random.uniform(-10, 10, 25).reshape((5, 5))
-a = a @ a.T
-print(a)
-print(np.linalg.eig(a))
-print(jacobi(Csr(a), 1))
