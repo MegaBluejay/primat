d96854f140a047735cf9667ff94efb7b6c6dd58c
MegaBluejay <dmoiseev2011@gmail.com>
2022-06-29 14:35:33 +0300

    Revert "add relaxation"
    This reverts commit 3e2d781c3461804fb3178248238979b6465c4c40.

--- primat/lab3/zeydel.py
+++ primat/lab3/zeydel.py
@@ -4,23 +4,22 @@ from primat.lab2.grad import norm_sq
 from primat.lab3.csr import Csr
 
 
-def zeydel(a, b, eps, om):
+def zeydel(a, b, eps):
     n = a.n
     xp = np.zeros(b.shape)
     x = b.copy()
     while norm_sq(x - xp) > eps**2:
         xp = x.copy()
         for i in range(n):
-            x[i] *= 1 - om
-            q = 0
+            x[i] = b[i]
             start, end = a.r[i : i + 2]
             for v, j in zip(a.v[start:end], a.c[start:end]):
                 if i != j:
-                    q += v * x[j]
-            x[i] += om * (b[i] - q) / a[i, i]
+                    x[i] -= v * x[j]
+            x[i] /= a[i, i]
     return x
 
 
 a = Csr([[1, 2], [3, 4]])
 b = np.array([1, 1])
-print(zeydel(a, b, 1e-5, 1.1))
+print(zeydel(a, b, 1e-5))
