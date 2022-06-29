b5b2e7f03b44f1d94d8d64a48d455d3a9226dd1d
MegaBluejay <dmoiseev2011@gmail.com>
2022-06-29 14:01:52 +0300

    fix zeydel

--- primat/lab3/zeydel.py
+++ primat/lab3/zeydel.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 from primat.lab2.grad import norm_sq
+from primat.lab3.csr import Csr
 
 
 def zeydel(a, b, eps):
@@ -10,5 +11,15 @@ def zeydel(a, b, eps):
     while norm_sq(x - xp) > eps**2:
         xp = x.copy()
         for i in range(n):
-            x[i] = (b[i] - sum(a[i, j] * x[j] if i != j else 0 for j in range(n))) / a[i, i]
+            x[i] = b[i]
+            start, end = a.r[i : i + 2]
+            for v, j in zip(a.v[start:end], a.c[start:end]):
+                if i != j:
+                    x[i] -= v * x[j]
+            x[i] /= a[i, i]
     return x
+
+
+a = Csr([[1, 2], [3, 4]])
+b = np.array([1, 1])
+print(zeydel(a, b, 1e-5))
