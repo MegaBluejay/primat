561ef0f9f86bb07286dfabd05369382a37e1efbb
murovv <murovgleb@gmail.com>
2022-06-30 16:02:20 +0300

    pics for lab3

--- primat/lab3/temperatureMap.py
+++ primat/lab3/temperatureMap.py
@@ -9,32 +9,27 @@ timeZeid = []
 timeLU = []
 ns = []
 ks =[]
-for k in range(0,4):
-    for n in range(0,100,10):
-        A, F = qq(n, k)
-        time1 = time()
-        x, count = z.zeydel(A,np.array(F),1)
-        time2 = time()
-        timeZeid.append(time2 - time1)
-        print(x)
-        time2 = time()
-        l, u = lu.decomp(A.transpose())
-        x = lu.solve(l, u, np.array(F))
-        time3 = time()
-        print(x)
-        ns.append(n)
-        ks.append(k)
-        timeLU.append(time3-time2)
+dxs = []
+for n in range(10,100,10):
+    A, F = gilbert(n)
+    time1 = time()
+    x, count = z.zeydel(A,np.array(F),0.01)
 
-plt.scatter(ks,ns,c=timeLU,cmap='Greens', label='LU')
-plt.colorbar()
-plt.legend()
-plt.xlabel('k')
-plt.ylabel('n')
+    time2 = time()
+    dx = np.mean([abs(x[i] - i - 1) for i in range(len(x))])
+    timeZeid.append(time2 - time1)
+    time2 = time()
+    l, u = lu.decomp(A.transpose())
+    x = lu.solve(l, u, np.array(F))
+    time3 = time()
+    ns.append(n)
+    dxs.append(dx)
+    timeLU.append(time3-time2)
+
+plt.plot(ns,timeLU)
 plt.show()
-plt.scatter(ks,ns,c=timeZeid,cmap='Reds', label='Zeydel')
-plt.colorbar()
-plt.legend()
-plt.xlabel('k')
-plt.ylabel('n')
+plt.plot(ns,timeZeid)
 plt.show()
+plt.plot(ns,dxs)
+plt.show()
+
