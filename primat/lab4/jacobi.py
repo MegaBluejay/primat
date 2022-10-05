0c5d629ba447a54e983d00054406ac584fc7919a
MegaBluejay <dmoiseev2011@gmail.com>
2022-10-05 16:42:16 +0300

    try with normal arrays

--- primat/lab4/jacobi.py
+++ primat/lab4/jacobi.py
@@ -1,63 +1,55 @@
 from bisect import bisect_right
-from math import sqrt, copysign, atan2, sin, cos
+from math import sqrt, copysign, atan2, sin, cos, isclose
+from functools import cache
+
+import numpy as np
 
 from primat.lab3.csr import Csr
 
 
-def non_diag(a):
-    for i in range(a.n):
-        start, end = a.r[i : i + 2]
-        for q in range(start, end):
-            if a.c[q] == i:
-                continue
-            yield q, a.v[q]
-
-
-def next_jac(a):
-    q = max(non_diag(a), key=lambda t: abs(t[1]), default=(-1, 0))[0]
-    j, k = sorted([bisect_right(a.r, q) - 1, a.c[q]])
-    if a[j, j] == a[k, k]:
-        s, c = 1 / sqrt(2), 1 / sqrt(2)
+def rotate(a, p, j, k):
+    if isclose(a[j, j], a[k, k], rel_tol=1e-20):
+        s, c = [1 / sqrt(2)] * 2
+        tau = np.pi / 4
     else:
-        tau = 2 * a[j, k] / (a[j, j] - a[k, k])
-        y = 1 / sqrt(1 + tau**2)
-        s, c = copysign(sqrt((1 - y) / 2), tau), sqrt((1 + y) / 2)
+        tau = (a[j, j] - a[k, k]) / (2 * a[j, k])
+        t = copysign(1, tau) / (abs(tau) + sqrt(1 + tau**2))
+        c = 1 / sqrt(1 + t**2)
+        s = c * t
+    new = np.copy(a)
+    new[j, j] = c**2 * a[j, j] - 2 * s * c * a[j, k] + s**2 * a[k, k]
+    new[k, k] = s**2 * a[j, j] + 2 * s * c * a[j, k] + c**2 * a[k, k]
+    new[j, k] = new[k, j] = (c**2 - s**2) * a[j, k] + s * c * (a[k, k] - a[j, j])
+    for m in range(a.shape[0]):
+        if m in [j, k]:
+            continue
+        new[j, m] = new[m, j] = c * a[j, m] - s * a[k, m]
+        new[k, m] = new[m, k] = s * a[j, m] + c * a[k, m]
+    for i in range(a.shape[0]):  # Update transformation matrix
+        temp = p[i, j]
+        p[i, j] = temp - s * (p[i, k] + tau * p[i, j])
+        p[i, k] = p[i, k] + s * (temp - tau * p[i, k])
+    return new, p
 
-    b = Csr(([], [], [0], a.n))
-    for i in range(a.n):
-        for m in range(i):
-            x = b[m, i]
-            if x:
-                b.v.append(x)
-                b.c.append(m)
-        for m in range(i, a.n):
-            if [i, m] == [j, j]:
-                x = c**2 * a[j, j] - 2 * s * c * a[j, k] + s**2 * a[k, k]
-            elif [i, m] == [k, k]:
-                x = s**2 * a[j, j] + 2 * s * c * a[j, k] + c**2 * a[k, k]
-            elif [i, m] == [j, k]:
-                x = (c**2 - s**2) * a[j, k] + s * c * (a[k, k] - a[j, j])
-            elif j in [i, m]:
-                x = c * a[j, m] - s * a[k, m]
-            elif k in [i, m]:
-                x = s * a[j, m] + c * a[k, m]
-            else:
-                x = a[i, m]
-            if x:
-                b.v.append(x)
-                b.c.append(m)
-        b.r.append(len(b.v))
-    return b
+
+# def jacobi(a, eps):
+#     while abs(max(non_diag(a), key=lambda t: abs(t[1]), default=(-1, 0))[1]) >= eps:
+#         a = rotate(a)
+#     return [a[i, i] for i in range(a.n)]
 
 
 def jacobi(a, eps):
-    while abs(max(non_diag(a), key=lambda t: abs(t[1]), default=(-1, 0))[1]) >= eps:
-        a = next_jac(a)
-    return [a[i, i] for i in range(a.n)]
+    mask = ~np.eye(a.shape[0], dtype=bool)
+    p = np.eye(a.shape[0], dtype=float)
+    while abs(a[q := np.unravel_index(np.argmax(np.abs(np.where(mask, a, 0))), a.shape)]) >= eps:
+        a, p = rotate(a, p, *q)
+    print(a)
+    return a.diagonal(), p
 
 
-import numpy as np
-
-a = np.array([[4, -30, 60, -35], [-30, 300, -675, 420], [60, -675, 1620, -1050], [-35, 420, -1050, 700]])
+a = np.array([[4, -30, 60, -35], [-30, 300, -675, 420], [60, -675, 1620, -1050], [-35, 420, -1050, 700]], dtype=float)
 print(a)
-print(jacobi(Csr(a), 1e-4))
+vals, vecs = jacobi(a, 1e-9)
+print(vals)
+print(vecs)
+print(a @ vecs[:, 0], vecs[:, 0] * vals[0])
